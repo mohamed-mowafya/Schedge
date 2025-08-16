b8a3c0187899508a46886b2eaecd66dc1fbbcab2
mohamed-mowafya <mohamedmowafy1999@icloud.com>
2025-08-16 10:16:14 -0400

    Add error handling in case of copy clipboard error

--- www/src/hooks/useCopyLink.ts
+++ www/src/hooks/useCopyLink.ts
@@ -1,8 +1,12 @@
+import { toast } from "react-toastify";
+
 export function useCopyLink(sessionUUID?: string) {
   const copyLink = () => {
     if (!sessionUUID) return;
     const shareUrl = `${window.location.origin}/calendar/${sessionUUID}`;
-    navigator.clipboard.writeText(shareUrl);
+    navigator.clipboard.writeText(shareUrl).catch(() => {
+      toast.error("An error has occurred while copying the calendar's url.");
+    });
   };
   return { copyLink };
 }
